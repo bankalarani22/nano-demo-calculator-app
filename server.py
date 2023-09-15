0037e96e9633a66abc4850dc437ae6db81f61a10
bankalarani22 <144934830+bankalarani22@users.noreply.github.com>
2023-09-15 13:27:17 +0000

    modified code

--- server.py
+++ server.py
@@ -2,18 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return "Welcome to the calculator app!"
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    return "This is the addition route."
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    return "This is the subtraction route."
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
+
